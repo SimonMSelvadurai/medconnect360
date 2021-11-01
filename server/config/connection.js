2e31bd9184bca467f35889079e5385acccc75ed0
SimonMSelvadurai <simon.selvadurai0607@gmail.com>
2021-11-02 00:18:28 +1100

    Added try-catch for async functions to fix herouk build

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 require('dotenv').config()
-//const connectionString = "mongodb+srv://Johan123:O8PaM3qsTozcgxz1@cluster0.untky.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const dbURI = process.env.MONGODB_URI ||'mongodb://localhost/medconnect360'
 console.log('DB URI', dbURI)
 mongoose.connect(dbURI, {
